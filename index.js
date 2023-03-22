01b88a0c42b89f51ab5d09e3f70d84da3497df6d
Patricia <wambuipatricia3@gmail.com>
2023-03-22 15:03:54 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 
-function addingEventListener() {
+/*function addingEventListener() {
     const button = document.getElementById('button');
     button.addEventListener('click', function(event) {
       console.log('button clicked!');
     });
-  }
+  }*/
+
+function addingEventListener(){
+  const button=document.getElementById('button');
+  button.addEventListener('click',function(event) {
+    console.log ('I have been clicked!');
+  });
+}
+  
